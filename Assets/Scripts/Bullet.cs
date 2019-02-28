32312b321319cdb5b82f331aaf26b8524db3de84
summitdavid <dg@summitstartups.com>
2019-02-28 12:56:38 -0800

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider col){
+        Player p = col.GetComponent<Player>();
+        if(p){
+            p.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2;
+    public Camera camera;
+    public GameObject Bullet;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * speed * Time.deltaTime;
+        RaycastHit hit;
+        if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit, 50))
+        {
+            transform.LookAt(new Vector3(hit.point.x, 0.5f, hit.point.z));
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click();
+        }
+    }
+
+    void Click()
+    {
+        Instantiate(Bullet, transform.position + transform.forward, transform.rotation);
+    }
+
+    public void GameOver()
+    {
+        transform.position = Vector3.up * 0.5f;
+    }
+}
